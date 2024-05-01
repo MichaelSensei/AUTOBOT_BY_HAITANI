6f5d36f7c0f911da1a152ffc8103979134130510
MichaelSensei <165430298+MichaelSensei@users.noreply.github.com>
2024-05-01 14:58:37 +0000

    Update help.js

--- script/help.js
+++ script/help.js
@@ -25,13 +25,13 @@ module.exports.run = async function({
       let page = 1;
       let start = (page - 1) * pages;
       let end = start + pages;
-      let helpMessage = `Command List:━━━━━━━━━━━━━\n━━━━━━━━━━━━━\n`;
+      let helpMessage = `Command List:━━━━━━━━━━━━━\n\n`;
       for (let i = start; i < Math.min(end, commands.length); i++) {
-        helpMessage += `\t${i + 1}. ➪ ${prefix}${commands[i]} ❘⸙\n`;
+        helpMessage += `\t${i + 1}. ➪ ${commands[i]} ❍\n`;
       }
       helpMessage += '\nEvent List:\n\n';
       eventCommands.forEach((eventCommand, index) => {
-        helpMessage += `\t${index + 1}. 「 ${prefix}${eventCommand} 」\n`;
+        helpMessage += `\t${index + 1}. ➪ ${eventCommand} ❍\n`;
       });
       helpMessage += `\nPage ${page}/${Math.ceil(commands.length / pages)}. To view the next page, type '${prefix}help page number'. To view information about a specific command, type '${prefix}help command name'.`;
       api.sendMessage(helpMessage, event.threadID, event.messageID);
@@ -40,9 +40,9 @@ module.exports.run = async function({
       const pages = 20;
       let start = (page - 1) * pages;
       let end = start + pages;
-      let helpMessage = `Command List:━━━━━━━━━━━━━\n━━━━━━━━━━━━━\n`;
+      let helpMessage = `Command List:━━━━━━━━━━━━━\n\n`;
       for (let i = start; i < Math.min(end, commands.length); i++) {
-        helpMessage += `\t${i + 1}. ➪ ${prefix}${commands[i]} ❘⸙\n`;
+        helpMessage += `\t${i + 1}. ➪ ${commands[i]} ❍\n`;
       }
       helpMessage += '\nEvent List:\n\n';
       eventCommands.forEach((eventCommand, index) => {
